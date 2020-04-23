078c86794920b1405001ba8756f41be410aacc66
Alan Dsilva <deathbringer2604@gmail.com>
2020-04-23 17:49:00 +0530

    Add list view
    Future prospect may include adding data dynamically through api requests

---
+++ star_wars_listview/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:starwarslistview/models/starWarsTile.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<StarWarsFilm> _filmList = [
+    StarWarsFilm(
+      imgUrl: "t",
+      title: "Episode I - The Phantom Menance(1999)",
+      description: "t",
+    ),
+    StarWarsFilm(
+      imgUrl: "t",
+      title: "Episode II – Attack of the Clones (2002)",
+      description: "t",
+    ),
+    StarWarsFilm(
+      imgUrl: "t",
+      title: "Episode III – Revenge of the Sith (2005)",
+      description: "",
+    ),
+    StarWarsFilm(
+      imgUrl: "t",
+      title: "Episode IV – A New Hope (1977)",
+      description: "t",
+    ),
+    StarWarsFilm(
+      imgUrl: "t",
+      title: "Episode V – The Empire Strikes Back (1980)",
+      description: "",
+    ),
+    StarWarsFilm(
+      imgUrl: "t",
+      title: "Episode VI – Return of the Jedi (1983)",
+      description: "t",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Star Wars Films'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: ExpansionTile(
+              title: Text(_filmList[index].title),
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(_filmList[index].title),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: _filmList.length,
+      ),
+    );
+  }
+}
---
+++ star_wars_listview/lib/models/starWarsTile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+
+class StarWarsFilm {
+  final String imgUrl;
+  final String title;
+  final String description;
+
+  StarWarsFilm({
+    @required this.imgUrl,
+    @required this.title,
+    @required this.description,
+  });
+}
